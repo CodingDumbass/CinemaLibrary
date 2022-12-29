afb93dcdae956bb878be34cc69438d79d8d64919
CodingDumbass <81563209+CodingDumbass@users.noreply.github.com>
2022-12-29 14:46:53 +0200

    Add files via upload

---
+++ CinemaLibrary.Infrastructure/Models/Favourite.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CinemaLibrary.Infrastructure.Models
+{
+    public partial class Favourite
+    {
+        public Guid FavouriteId { get; set; }
+        public Guid MovieId { get; set; }
+    }
+}
---
+++ CinemaLibrary.Infrastructure/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CinemaLibrary.Infrastructure.Models
+{
+    public partial class Movie
+    {
+        public Guid MovieId { get; set; }
+        public Guid CategoryId { get; set; }
+        public int MovieLength { get; set; }
+        public float MovieRating { get; set; }
+        public string MovieName { get; set; } = null!;
+    }
+}
